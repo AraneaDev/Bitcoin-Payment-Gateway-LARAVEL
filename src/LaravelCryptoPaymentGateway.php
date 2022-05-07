f3ff37f4f3307dbd2552ebcd7f9fa8c8ce9042b9
Aranea Development <info@aranea-development.nl>
2022-05-07 12:43:11 +0200

    Update LaravelCryptoPaymentGateway.php

--- src/LaravelCryptoPaymentGateway.php
+++ src/LaravelCryptoPaymentGateway.php
@@ -181,7 +181,7 @@ class LaravelCryptoPaymentGateway
         $options['previous'] = url()->previous();
         session(["paymentbox_{$payment_session_id}" => $options]);
 
-        $paymentbox_url = action(\Victorybiz\LaravelCryptoPaymentGateway\Http\Controllers\CryptoPaymentController::class, ['cryptopsid' => $payment_session_id]);
+        $paymentbox_url = action(\\\\Victorybiz\LaravelCryptoPaymentGateway\Http\Controllers\CryptoPaymentController::class, ['cryptopsid' => $payment_session_id]);
         
         return $paymentbox_url;
     }
